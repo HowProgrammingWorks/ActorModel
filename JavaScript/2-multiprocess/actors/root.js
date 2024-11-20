07b72f42100c42096c1944792611fb32d3cf2d2b
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2024-11-20 11:47:14 +0200

    Update multi process example

--- JavaScript/2-multiprocess/actors/root.js
+++ JavaScript/2-multiprocess/actors/root.js
@@ -2,20 +2,22 @@
 
 const ActorSystem = require('../system');
 
-ActorSystem.register(class Root {
-  constructor() {
-    console.log('Start actor: Root');
-    ActorSystem.start('Monitoring');
-    ActorSystem.start('Renderer');
-    ActorSystem.start('Mailer', 3);
-  }
+ActorSystem.register(
+  class Root {
+    constructor() {
+      console.log('Start actor: Root');
+      ActorSystem.start('Monitoring');
+      ActorSystem.start('Renderer');
+      ActorSystem.start('Mailer', 3);
+    }
 
-  message() {}
+    message() {}
 
-  exit() {
-    ActorSystem.stop('Monitoring');
-    ActorSystem.stop('Renderer');
-    ActorSystem.stop('Mailer');
-    console.log('Stop actor: Root');
-  }
-});
+    exit() {
+      ActorSystem.stop('Monitoring');
+      ActorSystem.stop('Renderer');
+      ActorSystem.stop('Mailer');
+      console.log('Stop actor: Root');
+    }
+  },
+);
